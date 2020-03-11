1b0a1b99f0e57b76b1f1d69b940ba8f14a078105
Veronika <v.vasilchykava@gmail.com>
2020-03-11 11:40:24 +0300

    Add a folder sources to my project

---
+++ sources/models/activitytypes.js
@@ -0,0 +1,4 @@
+export const activitytypes = new webix.DataCollection({
+	url: "http://localhost:8096/api/v1/activitytypes/",
+	save: "http://localhost:8096/api/v1/activitytypes/"
+});
---
+++ sources/models/contacts.js
@@ -0,0 +1,4 @@
+export const contacts = new webix.DataCollection({
+	url: "http://localhost:8096/api/v1/contacts/",
+	save: "http://localhost:8096/api/v1/contacts/"
+});
