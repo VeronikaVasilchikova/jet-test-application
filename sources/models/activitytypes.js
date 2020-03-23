1b08ad845607833f488b7c8699cdc5940f1f4d41
VeronikaVasilchikova <veroni4ka.v@gmail.com>
2020-03-23 13:28:38 +0300

    Update before merging with master

--- sources/models/activitytypes.js
+++ sources/models/activitytypes.js
@@ -1,15 +1,6 @@
-// export const activitytypes = new webix.DataCollection({
-// 	url: "http://localhost:8096/api/v1/activitytypes/",
-// 	save: "rest->http://localhost:8096/api/v1/activitytypes/",
-// 	scheme: {
-// 		$init: (obj) => {
-// 			obj.value = obj.Value;
-// 		}
-// 	}
-// });
-
 export const activitytypes = new webix.DataCollection({
-	data: [{"id":7,"Value":"Meeting","Icon":"flag"},{"id":9,"Value":"Chat","Icon":"comment"},{"id":13,"Value":"Lunch","Icon":"clock"},{"id":15,"Value":"Phone call","Icon":"phone"}],
+	url: "http://localhost:8096/api/v1/activitytypes/",
+	save: "rest->http://localhost:8096/api/v1/activitytypes/",
 	scheme: {
 		$init: (obj) => {
 			obj.value = obj.Value;
--- sources/models/statuses.js
+++ sources/models/statuses.js
@@ -1,13 +1,4 @@
-// export const statuses = new webix.DataCollection({
-// 	url: "http://localhost:8096/api/v1/statuses/",
-// 	save: "rest->http://localhost:8096/api/v1/statuses/"
-// });
-
 export const statuses = new webix.DataCollection({
-	data: [{id: 1, Value: "In Progress", Icon: "cogs"}, {id: 2, Value: "Active", Icon: "user"}, {id: 3, Value: "Waiting", Icon: "pencil"}, {id: 4, Value: "Closed", Icon: "plus"}]
-	// scheme: {
-	// 	$init: (obj) => {
-	// 		obj.value = obj.Value;
-	// 	}
-	// }
+	url: "http://localhost:8096/api/v1/statuses/",
+	save: "rest->http://localhost:8096/api/v1/statuses/"
 });
